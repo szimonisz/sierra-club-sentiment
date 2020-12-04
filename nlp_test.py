2f94376887686d7a9686b3351296e88f5ceffb1e
Kelemen Szimonisz via Macbook Pro <kelemen.szimonisz@macbookpro>
2020-12-03 23:46:30 -0800

    model developed

--- nlp_test.py
+++ nlp_test.py
@@ -14,7 +14,11 @@ from nltk.tag import pos_tag
 from nltk.corpus import twitter_samples
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import stopwords
-import re, string
+from nltk import FreqDist
+from nltk import classify
+from nltk import NaiveBayesClassifier
+from nltk.tokenize import word_tokenize
+import re, string, random
 
 # normalization is the porcess of reducing a word to its simplest form
 # example of normalization: ("ran","runs","running" = "run")
@@ -22,13 +26,26 @@ import re, string
 # stemming and lemmatization are forms of normalization
 # Stemming: removes affixes from a word. 
 # Lemmatization: similiar to stemming but considers context of the words (part of speech, tense)
+# lemmatization considers part of speech (pos)
 
 # Why normalize the data?
 # we want our algorithm to understand these 3 words as one, in order to simplify algorithm and reduce number of 'features' (independent variables)
 
 # algorithm's dependent variables (classifiers): positive and negative
 # algorithm's indendent variables (features): a tweet's text
- 
+
+# create dictionary with keys -> tokens and values -> True
+# (this is a requirement for NLTK's Naive Bayes classifier function) 
+
+def get_tweets_for_model(cleaned_tokens_list):
+    for tweet_tokens in cleaned_tokens_list:
+        yield dict([token, True] for token in tweet_tokens)
+
+def get_all_words(cleaned_tokens_list):
+    for tokens in cleaned_tokens_list:
+        for token in tokens:
+            yield token
+
 def remove_noise(tweet_tokens, stop_words = ()):
     cleaned_tokens = []
 
@@ -61,7 +78,9 @@ positive_tweets = twitter_samples.strings('positive_tweets.json')
 negative_tweets = twitter_samples.strings('negative_tweets.json')
 text = twitter_samples.strings('tweets.20150430-223406.json')
 
-tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
+positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
+negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
+
 
 print("\nPositive tweet (untokenized):")
 print(positive_tweets[0])
@@ -69,10 +88,55 @@ print("\nPositive tweet (tokenized / part-of-speech tagged):")
 #note: pos_tag stands for 'part-of-speech tagger'
 # part of speech: "In English the main parts of speech are noun, pronoun, adjective, determiner, verb, adverb, preposition, conjunction, and interjection."
 
-print(pos_tag(tweet_tokens[0]))
+print(pos_tag(positive_tweet_tokens[0]))
 
 print("\nPositive tweet (lemmatized / noise removed):")
 # a stop word is the kind of word a search engine ignores to be efficient (is, at, my, I, be, etc.)
 stop_words = stopwords.words('english')
-print(remove_noise(tweet_tokens[0], stop_words))
+print(remove_noise(positive_tweet_tokens[0], stop_words))
 
+# an array of arrays
+positive_cleaned_tokens_list = []
+negative_cleaned_tokens_list = []
+
+for tokens in positive_tweet_tokens:
+    positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+
+for tokens in negative_tweet_tokens:
+    negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+
+print("\n 500th positive tweet vs cleaned 500th positive tweet:")
+print(positive_tweet_tokens[500])
+print(positive_cleaned_tokens_list[500])
+
+all_positive_words = get_all_words(positive_cleaned_tokens_list)
+freq_dist_positive = FreqDist(all_positive_words)
+
+print("\nTop 10 Most common words found in all positive tweets")
+print(freq_dist_positive.most_common(10))
+
+positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
+negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
+
+positive_dataset = [(tweet_dict, "Positive") for tweet_dict in positive_tokens_for_model]
+negative_dataset = [(tweet_dict, "Negative") for tweet_dict in negative_tokens_for_model]
+
+dataset = positive_dataset + negative_dataset
+random.shuffle(dataset)
+train_data = dataset[:7000]
+test_data = dataset[7000:]
+
+classifier = NaiveBayesClassifier.train(train_data)
+
+print("Accuracy is:", classify.accuracy(classifier, test_data))
+
+print(classifier.show_most_informative_features(10))
+
+custom_tweet = "This place sucks! I can't believe how rude these people are."
+custom_tokens = remove_noise(word_tokenize(custom_tweet))
+print("\nCustom tweet: " + custom_tweet)
+print(classifier.classify(dict([token, True] for token in custom_tokens)))
+custom_tweet2 = "This store is amazing! Best place in town."
+custom_tokens2 = remove_noise(word_tokenize(custom_tweet2))
+print("\nCustom tweet: " + custom_tweet2)
+print(classifier.classify(dict([token, True] for token in custom_tokens2)))
