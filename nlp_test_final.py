2bf9170a4e3c7df87555aef962ded2bfe2c5a62a
Kelemen Szimonisz via Macbook Pro <kelemen.szimonisz@macbookpro>
2020-12-06 19:59:11 -0800

    finished!

--- nlp_test_final.py
+++ nlp_test_final.py
@@ -1,47 +1,27 @@
 # http://www.nltk.org/howto/twitter.html
-# importing modules (python-term) from the nltk library
 
 from nltk.tag import pos_tag
 from nltk.corpus import twitter_samples
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import stopwords
-from nltk import FreqDist
-from nltk import classify
-from nltk import NaiveBayesClassifier
 from nltk.tokenize import word_tokenize
-import re, string, random
-import json
+from nltk import FreqDist, classify, NaiveBayesClassifier
+import re, string, random, json
 
-# normalization is the process of reducing a word to its simplest form
-# example of normalization: ("ran","runs","running" = "run")
+# Normalization: the process of reducing a word to its simplest form
+# Example of normalization: ("ran","runs","running" = "run")
 
-# stemming and lemmatization are forms of normalization
+# Stemming and Lemmatization are forms of normalization
 # Stemming: removes affixes from a word. 
 # Lemmatization: similiar to stemming but considers context of the words (part of speech, tense)
 
 # Why normalize the data?
-# we want our algorithm to understand these 3 words as one, in order to simplify algorithm and reduce number of 'features' (independent variables)
-
-# algorithm's dependent variables (classifiers): positive and negative
-# algorithm's indendent variables (features): a tweet's text
-
-# create dictionary with keys -> tokens and values -> True
-# (this is a requirement for NLTK's Naive Bayes classifier function) 
-
-def get_tweets_for_model(cleaned_tokens_list):
-    for tweet_tokens in cleaned_tokens_list:
-        yield dict([token, True] for token in tweet_tokens)
-
-def get_all_words(cleaned_tokens_list):
-    for tokens in cleaned_tokens_list:
-        for token in tokens:
-            yield token
+# We want our algorithm to understand these 3 words as one, in order to simplify algorithm and reduce number of 'features' (independent variables)
 
 def remove_noise(tweet_tokens, stop_words = ()):
     cleaned_tokens = []
 
-    #note: pos_tag stands for 'part-of-speech tagger'
-    # part of speech: "In English the main parts of speech are noun, pronoun, adjective, determiner, verb, adverb, preposition, conjunction, and interjection."
+    #pos_tag stands for 'part-of-speech tagger'
     for token, tag in pos_tag(tweet_tokens):
         # if token is a hyperlink, replace it with an empty string
         token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
@@ -64,32 +44,41 @@ def remove_noise(tweet_tokens, stop_words = ()):
             cleaned_tokens.append(token.lower())
     return cleaned_tokens
 
+# algorithm's dependent variables (classifications): positive and negative
+# algorithm's indendent variables (features): a tweet's text
+# create a dictionary with keys -> tokens and values -> True
+# (this is a requirement for NLTK's Naive Bayes classifier function) 
+def get_tweets_for_model(cleaned_tokens_list):
+    for tweet_tokens in cleaned_tokens_list:
+        yield dict([token, True] for token in tweet_tokens)
+
+def get_all_words(cleaned_tokens_list):
+    for tokens in cleaned_tokens_list:
+        for token in tokens:
+            yield token
+
+def load_json_into_array(json_filename):
+	json_object_list = []
+        # each line in the json file contains a seperate json object
+	with open(json_filename) as f:
+		for line in f.readlines():
+			if not line.strip(): # skip empty lines
+				continue
+			json_object_list.append(json.loads(line))
+	return json_object_list
 
 if __name__ == "__main__":
-	positive_tweets = twitter_samples.strings('positive_tweets.json')
-	negative_tweets = twitter_samples.strings('negative_tweets.json')
 	positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
 	negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
 
-	sierra_club_tweet_data_2019, sierra_club_tweet_data_2020 = [], []
-        # each line in the json file contains a seperate json object
-	with open('./sierraclub_2019-06_to_2019-12.json') as f:
-		for line in f.readlines():
-			if not line.strip(): # skip empty lines
-				continue
-			sierra_club_tweet_data_2019.append(json.loads(line))
-	with open('sierraclub_2020-06_to_2020-12.json') as f:
-		for line in f.readlines():
-			if not line.strip(): # skip empty lines
-				continue
-			sierra_club_tweet_data_2020.append(json.loads(line))
+	sierra_club_tweet_data_2019 = load_json_into_array('./sierraclub_2019-06_to_2019-12.json')
+	sierra_club_tweet_data_2020 = load_json_into_array('./sierraclub_2020-06_to_2020-12.json')
 
+        # tokenize each tweet text
 	tokenized_tweets_2019, tokenized_tweets_2020 = [], []
 	for tweet in sierra_club_tweet_data_2019:
-		#print(tweet)
 		tweet_text = tweet['tweet']
 		tokenized_tweets_2019.append(remove_noise(word_tokenize(tweet_text)))
-
 	for tweet in sierra_club_tweet_data_2020:
 		tweet_text = tweet['tweet']
 		tokenized_tweets_2020.append(remove_noise(word_tokenize(tweet_text)))
@@ -102,11 +91,6 @@ if __name__ == "__main__":
 	    positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
 	for tokens in negative_tweet_tokens:
 	    negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
-
-	#all_positive_words = get_all_words(positive_cleaned_tokens_list)
-	#freq_dist_positive = FreqDist(all_positive_words)
-	#print("\nTop 10 Most common words found in all positive tweets")
-	#print(freq_dist_positive.most_common(10))
 
 	cleaned_tokens_list_2019, cleaned_tokens_list_2020 = [], []
 	for tokens in tokenized_tweets_2019:
@@ -125,21 +109,41 @@ if __name__ == "__main__":
 	train_data = dataset[:7000]
 	test_data = dataset[7000:]
 
-	classifier = NaiveBayesClassifier.train(train_data)
+	classifier = NaiveBayesClassifier.train(dataset)
 
 	total_tweets_2019 = len(cleaned_tokens_list_2019)
 	total_tweets_2020 = len(cleaned_tokens_list_2020)
-	print("Total tweets 06/01/2019 -> 12/01/2019: " + str(total_tweets_2019))
-	print("Total tweets 06/01/2020 -> 12/01/2020: " + str(total_tweets_2020))
 	positive_count_2019, positive_count_2020 = 0, 0
 
+	blm_count_2019, blm_count_2020 = 0,0
+	blm_positive_2019, blm_positive_2020 = 0,0
+
 	for tweet_tokens in cleaned_tokens_list_2019:
+		if "blm" in tweet_tokens:
+			blm_count_2019 += 1
 		if classifier.classify(dict([token, True] for token in tweet_tokens)) == "Positive":
 			positive_count_2019 += 1
+			if "blm" in tweet_tokens:
+				blm_positive_2019 += 1
+
 	for tweet_tokens in cleaned_tokens_list_2020:
+		if "blm" in tweet_tokens:
+			blm_count_2020 += 1
 		if classifier.classify(dict([token, True] for token in tweet_tokens)) == "Positive":
 			positive_count_2020 += 1
-	print("Positive tweet percentage in 2019: ")
-	print(positive_count_2019 / total_tweets_2019)
-	print("Positive tweet percentage in 2020: ")
-	print(positive_count_2020 / total_tweets_2020)
+			if "blm" in tweet_tokens:
+				blm_positive_2020 += 1
+
+	print("Total tweets (06/01/2019 -> 12/01/2019): " + str(total_tweets_2019))
+	print("Total tweets (06/01/2020 -> 12/01/2020): " + str(total_tweets_2020))
+	print("Total POSITIVE tweets (06/01/2019 -> 12/01/2019): " + str(positive_count_2019))
+	print("Total POSITIVE tweets (06/01/2020 -> 12/01/2020): " + str(positive_count_2020))
+
+	print("Positive tweet percentage (06/01/2019 -> 12/01/2019): " + str(positive_count_2019 / total_tweets_2019))
+	print("Positive tweet percentage (06/01/2020 -> 12/01/2020): " + str(positive_count_2020 / total_tweets_2020))
+
+	print("Total number of tweets that mention BLM (06/01/2019 -> 12/01/2019): "+ str(blm_count_2019))
+	print("Total number of POSITIVE tweets that mention BLM (06/01/2019 -> 12/01/2019): "+ str(blm_positive_2019))
+	print("\nTotal number of tweets that mention BLM (06/01/2020 -> 12/01/2020): "+ str(blm_count_2020))
+	print("Total number of POSITIVE tweets that mention BLM (06/01/2019 -> 12/01/2019): "+ str(blm_positive_2020))
+
